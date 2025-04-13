7eddb297ea538be34a5f2445376f90ef2ecf4b27
Gideon Grinberg <grinberg1165@gmail.com>
2025-04-13 22:31:38 +0200

    Remove AWS scripts

--- scripts/build_container.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 116453493772.dkr.ecr.us-east-1.amazonaws.com
-sudo docker build -t gideongrinberg/pipeline .
-sudo docker tag gideongrinberg/pipeline:latest 116453493772.dkr.ecr.us-east-1.amazonaws.com/gideongrinberg/pipeline:latest
-sudo docker push 116453493772.dkr.ecr.us-east-1.amazonaws.com/gideongrinberg/pipeline:latest
