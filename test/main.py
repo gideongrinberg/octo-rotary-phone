03b6bebcce86433d10b8e00ab92916db29c81f78
Gideon Grinberg <grinberg1165@gmail.com>
2025-04-14 00:39:42 +0200

    Remove outdated test code

--- test/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('./src/')
-
-from pipeline import *
-
-target = {"ID":33398702,"ra":344.420449437894,"dec":-8.06747255937119,"sector":42,"camera":1,"ccd":1}
-# print(process_target(target))
-print(lambda_handler(target, None))
